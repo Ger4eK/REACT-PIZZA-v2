81ad4fc422c901b90b26bcdcedd91b5824a0899b
Ger4eK <ger4ek228@gmail.com>
2022-06-07 23:28:32 +0300

    added pizzas fetch by using mockapi

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
-import pizzas from './assets/pizzas.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://629facf58b939d3dc29d123b.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json);
+      });
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
